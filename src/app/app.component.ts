efe2b882a8b8a3d5be1a87ce80df8b48ddcffec3
Xerxes Anthony <xerxesanthony25@gmail.com>
2021-10-02 22:12:11 +0800

    Angular Calculator

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculator-Angular';
+  num1=0;
+  num2=0;
+  answer=0;
+  change1=false;
+  
+getFirst(value : any){
+  const val= parseInt(value);
+  this.num1=val;
+  console.log(val);
+}
+getSecond(value: any){
+  const val= parseInt(value);
+  this.num2=val;
+  console.log(val);
+}
+
+onClickAdd(){
+this.answer=this.num1 + this.num2;
+
+}
+onClickSubtract(){
+  this.answer=this.num1-this.num2;
+  
+}
+onClickMultiply(){
+  this.answer=this.num1*this.num2;
+  
+}
+  onClickDivision(){
+    this.answer=this.num1/this.num2;
+    
+  }
+  
+}
